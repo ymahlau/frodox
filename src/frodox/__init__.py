f752edf3fb0e4dc9b6ae2aecb0e2d0045fe09790
ymahlau <mahlauyannik@gmail.com>
2026-02-05 18:15:03 +0100

    refactor: rename field -> fields

--- src/frodox/__init__.py
+++ src/frodox/__init__.py
@@ -1,5 +1,5 @@
 from frodox.asm import angular_spectrum_method_1d
-from frodox.field import gaussian_wave_1d, plane_wave_1d
+from frodox.fields import gaussian_wave_1d, plane_wave_1d
 from frodox.structures import binary_grating_heights_1d
 from frodox.tea import thin_element_approximation_1d
 
